308474963fa166587c525e504965a5710df4d73a
khang <khanghugo123@gmail.com>
2019-12-25 21:38:36 -0500

    removes all the trace of subprocess method

--- reconnect.py
+++ reconnect.py
@@ -11,61 +11,15 @@ timeOut = 0.5 # this is in second, reminder: 1s = 1000ms
 reconnectTime = 1
 ignoreTime = 0.5
 
-#both of these values are for the subprocess method
-#pingCount = "3" # in case of loss packet, upping this number maybe good
-#timeOut = "50" # this was in milisecond. this depends on the consistency of your internet, 100 maybe too high. again, using another module can make things faster
-
-
 # you can play the lick if you reconnect, no kidding
 winBeep = [440, 250]
 errorMessage = ['General failure', 'Destination host unreachable', 'Request timed out'] # this may not needed because pythonping method doesn't need to check errors
 
-# some pre-defined commands to run later with subprocess module
-# pingCommand = f"ping {domainName} -n {pingCount} -w {timeOut}".split() # this is for subprocess method
 disconnectCommand = 'netsh wlan disconnect'.split()
 reconnectCommand = f'netsh wlan connect name="{networkName}"'.split()
 
 # just a quick head-up
 print("Script started")
-
-# script run definitely
-# if you run with subprocess method then just remove the comments
-'''
-while True:
-	try:
-		# this function starts to ping and "communicate" to pipe an output for calling later. this doesn't require any extra module. If i try to use os module, command prompt will pop ups
-		result = str(subprocess.Popen(pingCommand,stdout=subprocess.PIPE).communicate())
-		# print(result)
-
-		for m in errorMessage:
-
-			#while I was writing this, I thought about packet loss so I made this small mechanism to avoid it
-			count = result.count(m)
-
-			# in short, if all the packet fails, it guarantee a reconnect, while <100% will be ignored
-			if count == int(pingCount):
-				subprocess.call(disconnectCommand)
-				subprocess.call(reconnectCommand)
-				print("Reconnecting...")
-
-				# just a head up
-				winsound.Beep(winBeep[0], winBeep[1])
-				
-				# these sleep to prevent running to fast, but the module itself is slow anyway
-				time.sleep(reconnectTime)
-				print("Running...")
-				continue
-			# as mentioned, unless there is 100% packet loss, it wont reconnect
-			elif count < int(pingCount):
-				time.sleep(ignoreTime)
-
-				continue
-
-	except Exception as e:
-		#print(e)
-		print("Script stopped")
-		break
-'''
 
 # this is the pythonping method, faster because it is a dedicated module just for pinging while subprocess was made for multi-purposes
 while True:
